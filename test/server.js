b795ea32375a31fe826f8a008778e5c78cc2e5c7
Leonard Struck <leonardstruck@blockform.at>
2021-03-24 11:13:22 +0100

    wrote test for service metadata request

--- test/server.js
+++ test/server.js
@@ -1,11 +1,13 @@
 //Require dev-dependencies
 let chai = require('chai')
 let chaiHttp = require('chai-http')
+let chaiJsonSchema = require('chai-json-schema')
 
 let server = require('../reconciliation/server')
 let should = chai.should()
 
 chai.use(chaiHttp)
+chai.use(chaiJsonSchema)
 
 describe('Server', () => {
     it('should return 404 on GET *', (done) => {
@@ -13,6 +15,32 @@ describe('Server', () => {
             .get('/')
             .end((err, res) => {
                 res.should.have.status(404);
+                done();
+            })
+    })
+    it('should return the service metadata when no query/-ies are posted', (done) => {
+        chai.request(server)
+            .post('/reconcile')
+            .end((err, res) => {
+                let serviceMetadataSchema = {
+                    type: 'object',
+                    required: ["name", "identifierSpace", "schemaSpace"]
+                }
+                res.should.have.status(200);
+                res.body.should.be.jsonSchema(serviceMetadataSchema)
+                done();
+            })
+    })
+    it('should return the service metadata when no query/-ies are set as get params', (done) => {
+        chai.request(server)
+            .get('/reconcile')
+            .end((err, res) => {
+                let serviceMetadataSchema = {
+                    type: 'object',
+                    required: ["name", "identifierSpace", "schemaSpace"]
+                }
+                res.should.have.status(200);
+                res.body.should.be.jsonSchema(serviceMetadataSchema)
                 done();
             })
     })
