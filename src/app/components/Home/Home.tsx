956a314bc766b4235a6c53aa458017a5af1e6981
Leonard Struck <me@leonardstruck.com>
2021-03-27 18:44:34 +0100

    Image should work now

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import { Helmet } from 'react-helmet'; 
 
 import { Button, Card, Elevation } from "@blueprintjs/core";
+import Island from "../../../assets/island.svg";
+
+
 
 export default function Home() {
   return (
@@ -11,7 +14,7 @@ export default function Home() {
       <title>reconcIsle - Home</title>
     </Helmet>
     <Card elevation={Elevation.FOUR} className="centeredCard">
-      <img src="./assets/island.svg" />
+      <img src={Island} />
       <h3 className="bp3-heading" >reconcIsle</h3>
       <p className="bp3-text-large">A OpenRefine reconciliation service with a simple GUI.</p>
       <Link to="/open"><Button rightIcon="arrow-right" intent="primary" minimal={true} fill={true}>Let's get started</Button></Link>
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+import { app, Menu, BrowserWindow, ipcMain } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
@@ -10,7 +10,7 @@ const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     height: 800,
-    width: 700,
+    width: 700
   });
 
   // and load the index.html of the app.
@@ -44,3 +44,109 @@ app.on('activate', () => {
 
 //Auto-Update
 require('update-electron-app');
+
+//Menu
+const template = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Open',
+        accelerator: 'CmdOrCtrl+O',
+        click() { openFile(); }
+      },
+      {
+        label: 'Save',
+        accelerator: 'CmdOrCtrl+S',
+        click() { saveFile(); }
+      }
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Cut',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+      },
+      {
+        label: 'Copy',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+      },
+    ]
+  }
+];
+
+if (process.platform == 'darwin') {
+  var name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'About ' + name,
+        role: 'about'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Services',
+        role: 'services',
+        submenu: []
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Hide ' + name,
+        accelerator: 'Command+H',
+        role: 'hide'
+      },
+      {
+        label: 'Hide Others',
+        accelerator: 'Command+Alt+H',
+        role: 'hideothers'
+      },
+      {
+        label: 'Show All',
+        role: 'unhide'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() { app.quit(); }
+      },
+    ]
+  });
+}
+
+const menu = Menu.buildFromTemplate(template);
+//Menu.setApplicationMenu(menu);
