650432552e2ab84e8e445b3ab1e4e3773fce5e97
Leonard Struck <me@leonardstruck.com>
2021-03-27 19:00:25 +0100

    ipcRenderer works

--- src/renderer.ts
+++ src/renderer.ts
@@ -32,3 +32,4 @@ import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import "normalize.css/normalize.css"
 import "@blueprintjs/popover2/lib/css/blueprint-popover2.css";
+
