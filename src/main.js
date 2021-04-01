c364e136fafd5163ae9dd4d3250940d644669b8e
Leonard Struck <me@leonardstruck.com>
2021-04-02 00:27:29 +0200

    electron-updater

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, ipcMain, session } = require("electron");
 const { service } = require("./service");
 const { fileStore } = require("./fileStore");
 const { isDev } = require("electron-is-dev");
+require("update-electron-app");
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
