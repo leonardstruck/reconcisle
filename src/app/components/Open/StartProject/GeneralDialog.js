d61bce37f74564a180c1a9a99ba8f6c5b319d7bc
Leonard Struck <me@leonardstruck.com>
2021-04-01 15:47:52 +0200

    Start new Project View

---
+++ src/app/components/Open/StartProject/GeneralDialog.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import slugify from "slugify";
+import {
+	FormGroup,
+	InputGroup,
+	Callout,
+	Radio,
+	RadioGroup,
+} from "@blueprintjs/core";
+
+export const GeneralDialog = (props) => {
+	useEffect(() => {
+		if (
+			props.projectSettings.general.name !== "" &&
+			props.projectSettings.general.source !== ""
+		) {
+			props.setNextButtonDisabled(false);
+		} else {
+			props.setNextButtonDisabled(true);
+		}
+	});
+	const handleNameChange = (e) => {
+		const replaceSpace = e.currentTarget.value.replace(/\s+/g, "-");
+		const withoutSpace = slugify(replaceSpace, {
+			lower: true,
+			remove: /[*_+~,./()'"!:@]/g,
+		});
+		e.currentTarget.value = withoutSpace;
+		props.setProjectSettings({
+			...props.projectSettings,
+			general: { ...props.projectSettings.general, name: withoutSpace },
+		});
+	};
+	return (
+		<div>
+			<FormGroup label="Name" labelInfo="(required)">
+				<InputGroup
+					id="name"
+					onChange={handleNameChange}
+					value={props.projectSettings.general.name}
+				/>
+			</FormGroup>
+			<Callout title="Valid Project Names" intent="primary">
+				Supported characters for a project name are: letters, numbers and
+				dashes. Dashes must not be entered next to another.
+			</Callout>
+			<FormGroup label="Source">
+				<RadioGroup
+					selectedValue={props.projectSettings.general.source}
+					onChange={(e) => {
+						props.setProjectSettings({
+							...props.projectSettings,
+							general: {
+								...props.projectSettings.general,
+								source: e.currentTarget.value,
+							},
+						});
+					}}
+				>
+					<Radio
+						label="Postgres, MySQL, MariaDB, SQLite, Microsoft SQL Server"
+						value="database"
+					/>
+					<Radio label="CSV (coming soon...)" value="csv" disabled={true} />
+					<Radio label="Excel (coming soon...)" value="excel" disabled={true} />
+				</RadioGroup>
+			</FormGroup>
+		</div>
+	);
+};
