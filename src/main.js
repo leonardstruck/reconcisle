c511a0738a2cc81dc33b2229e9ce7a164b858b3e
Leonard Struck <me@leonardstruck.com>
2021-04-02 19:21:23 +0200

    Updates are working now

--- src/main.js
+++ src/main.js
@@ -2,15 +2,56 @@ const {
 	app,
 	BrowserWindow,
 	ipcMain,
-	session,
-	Notification,
-	dialog,
-	autoUpdater,
 	Menu,
+	autoUpdater,
+	dialog,
 } = require("electron");
 const { service } = require("./service");
 const { fileStore } = require("./fileStore");
-const { isDev } = require("electron-is-dev");
+const log = require("electron-log");
+
+const isDev = process.env.NODE_ENV === "development";
+
+// log console to logfile
+console.log = log.log;
+
+console.log(isDev ? "Running in dev-mode" : "Running in production-mode");
+
+//Updates
+if (!isDev) {
+	const server = "https://update-server-reconcisle.vercel.app";
+	const feed = `${server}/update/${process.platform}/${app.getVersion()}`;
+
+	autoUpdater.setFeedURL(feed);
+
+	setInterval(() => {
+		autoUpdater.checkForUpdates();
+	}, 10 * 60 * 1000);
+
+	autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+		const dialogOpts = {
+			type: "info",
+			buttons: ["Restart", "Later"],
+			title: "Application Update",
+			message: process.platform === "win32" ? releaseNotes : releaseName,
+			detail:
+				"A new version has been downloaded. Restart the application to apply the updates.",
+		};
+
+		dialog.showMessageBox(dialogOpts).then((returnValue) => {
+			if (returnValue.response === 0) autoUpdater.quitAndInstall();
+		});
+	});
+
+	autoUpdater.on("error", (message) => {
+		console.error("There was a problem updating the application");
+		console.error(message);
+	});
+
+	autoUpdater.on("update-available", (message) => {
+		console.log("Update available, downloading now");
+	});
+}
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) {
@@ -77,6 +118,20 @@ const template = [
 						title: "🔎 Checking for updates",
 						silent: true,
 					}).show();
+
+					autoUpdater.on("update-not-available", () => {
+						new Notification({
+							title: "No update available",
+							silent: true,
+						}).show();
+					});
+					autoUpdater.on("update-available", () => {
+						new Notification({
+							title: "New Update",
+							body: "🎉 Update available. Downloading ⌛️",
+							sound: "Breeze",
+						}).show();
+					});
 				},
 			},
 		],
@@ -145,45 +200,3 @@ ipcMain.on("service", async (event, arg) => {
 	const result = await service(arg.service, arg.method, arg.obj);
 	event.reply(arg.reqId, result);
 });
-
-// Updater
-if (isDev) {
-	const server = "https://update.electronjs.org";
-	const feed = `${server}/leonardstruck/reconcisle/${process.platform}-${
-		process.arch
-	}/${app.getVersion()}`;
-
-	autoUpdater.setFeedURL(feed);
-	setInterval(() => {
-		autoUpdater.checkForUpdates();
-	}, 10 * 60 * 1000);
-
-	autoUpdater.once("update-available", (ev, err) => {
-		new Notification({
-			title: "New Update",
-			body: "🎉 Update available. Downloading ⌛️",
-			sound: "Breeze",
-		}).show();
-	});
-
-	autoUpdater.on("error", (message) => {
-		new Notification({
-			title: "There was a problem updating the application",
-			body: message,
-		}).show();
-	});
-
-	autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
-		const dialogOpts = {
-			type: "info",
-			button: ["Restart", "Later"],
-			title: "🤘 Update downloaded",
-			message: process.platform === "win32" ? releaseNotes : releaseName,
-			detail:
-				"A new version has been downloaded. Restart the application to apply the updates.",
-		};
-		dialog.showMessageBox(dialogOpts).then((returnValue) => {
-			if (returnValue.response === 0) autoUpdater.quitAndInstall();
-		});
-	});
-}
