88cf457c49982ea4b2b92080a3d91b7b2a22f701
Leonard Struck <me@leonardstruck.com>
2021-04-07 16:33:52 +0200

    StartProject General now Modular

---
+++ src/modules/database/views/config/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Collapse } from "@blueprintjs/core";
+
+export const configView = () => {
+	return (
+		<div>
+			<h1>This is the config view</h1>
+		</div>
+	);
+};
