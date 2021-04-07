43cbc7dece71a38035441cf86b25999032e3ed6a
Leonard Struck <me@leonardstruck.com>
2021-04-07 11:45:09 +0200

    not necessary

--- src/app/components/Reconc/Initializing.js
+++
@@ -1,8 +0,0 @@
-import { NonIdealState } from "@blueprintjs/core";
-import React from "react";
-
-export const Initializing = () => {
-    return (
-	    <NonIdealState icon={<Spinner />} title="Loading Project" />;
-    )
-}
