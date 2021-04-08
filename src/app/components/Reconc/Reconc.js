0ffb8cbc544e37c6f081decc19da1b554e8f5eef
Leonard Struck <me@leonardstruck.com>
2021-04-08 18:39:48 +0200

    Status Animation

--- src/app/components/Reconc/Reconc.js
+++ src/app/components/Reconc/Reconc.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Lottie from "react-lottie-segments";
+import * as animationData from "../../assets/statusAnimation.json";
 import Helmet from "react-helmet";
 import {
 	Card,
@@ -11,95 +13,17 @@ import {
 import { Link, useLocation } from "react-router-dom";
 import { fileStoreHandler } from "../../services/fileStoreHandler";
 
-const Loading = (props) => {
-	const initialize = () => {
-		fileStoreHandler({
-			store: "project",
-			method: "checkIfDataAvailable",
-			obj: { name: props.project },
-		}).then((res) => {
-			console.log("checkIfDataAvailable returned ", res);
-			if (res.status === "ok") {
-				if (res.isDataAvailable) {
-					props.setDataState("founddata");
-				} else {
-					props.setDataState("firstdl");
-				}
-			} else {
-				props.setDataState("error");
-			}
-		});
-	};
-
-	const downloadData = () => {
-		fileStoreHandler({
-			store: "project",
-			method: "downloadData",
-			obj: { name: props.project },
-		}).then((res) => {
-			console.log(res);
-			if (res.status === "ok") {
-				props.setDataState("startservice");
-			} else {
-				props.setDataState("error");
-			}
-		});
-	};
-	switch (props.action) {
-		case "initialize":
-			initialize();
-			break;
-		case "download":
-			downloadData();
-			break;
-	}
-	return <NonIdealState icon={<Spinner />} title={props.title} />;
-};
-
-const Views = (props) => {
-	const [dataState, setDataState] = useState("");
-	switch (dataState) {
-		case "":
-			return (
-				<Loading
-					title="Initializing"
-					action="initialize"
-					project={props.project}
-					setDataState={setDataState}
-				/>
-			);
-		case "firstdl":
-			return (
-				<Loading
-					title="Downloading Data from Source"
-					action="download"
-					project={props.project}
-					setDataState={setDataState}
-				/>
-			);
-		case "startservice":
-			return (
-				<Loading
-					title="Starting Reconciliation Service"
-					action="start"
-					project={props.project}
-					setDataState={setDataState}
-				/>
-			);
-		default:
-			return (
-				<Callout intent="danger">
-					<h1>An Error occured</h1>
-				</Callout>
-			);
-	}
-};
-
 export const Reconc = (props) => {
 	function useQuery() {
 		return new URLSearchParams(useLocation().search);
 	}
 	let query = useQuery();
+
+	const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: animationData.default,
+	};
 	return (
 		<div>
 			<Helmet>
@@ -107,7 +31,11 @@ export const Reconc = (props) => {
 			</Helmet>
 			<Card elevation={Elevation.FOUR} className="centeredCard">
 				<h3 className="bp3-heading">{query.get("name")}</h3>
-				<Views project={query.get("name")} />
+				<NonIdealState
+					icon={<Lottie options={defaultOptions} height={200} width={200} />}
+					title="This is a title"
+					description="This is a description"
+				/>
 				<Link to="/Open">
 					<Button
 						icon="arrow-left"
